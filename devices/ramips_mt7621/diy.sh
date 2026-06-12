a8f73b4c8be1791d4a30a62522e7a4cb27878a1e
xu <493734247@qq.com>
2026-06-12 14:59:36 +0800

    mt7621

--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -11,3 +11,10 @@ sed -i "s/DEVICE_MODEL := HC5962$/DEVICE_MODEL := HC5962 \/ B70/" target/linux/r
 sed -i '/# start dockerd/,/# end dockerd/d' .config
 
 sed -i "s/--max-leb-cnt=96/--max-leb-cnt=128/g" target/linux/ramips/image/mt7621.mk
+
+# 固定 sing-box 版本为 1.12.23
+for singbox_mk in feeds/kiddin9/sing-box/Makefile package/feeds/kiddin9/sing-box/Makefile; do
+	[ -f "$singbox_mk" ] || continue
+	sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=1.12.23/' "$singbox_mk"
+	sed -i 's/^PKG_HASH:=.*/PKG_HASH:=skip/' "$singbox_mk"
+done
